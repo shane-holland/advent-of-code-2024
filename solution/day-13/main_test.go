517c65d38bb72a0f5f2630da9df029b58e0c0c65
Shane Holland <shane.holland@ahead.com>
2024-12-13 08:42:09 -0500

    Day 13: Claw Contraption
    - Added solution for parts 1 and 2

---
+++ solution/day-13/main_test.go
@@ -0,0 +1,28 @@
+package day13
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"shaneholland.dev/aoc-2024/util"
+)
+
+const PART_1_EXPECTED = "480"
+const PART_2_EXPECTED = "875318608908"
+const PUZZLE_INPUT_PATH = "../../data/day-13/test.txt"
+
+func TestPart1(t *testing.T) {
+	testInput := util.ReadFile(PUZZLE_INPUT_PATH)
+	solver := Puzzle{}
+	answer1, _ := solver.Solve(testInput)
+
+	assert.Equal(t, PART_1_EXPECTED, answer1)
+}
+
+func TestPart2(t *testing.T) {
+	testInput := util.ReadFile(PUZZLE_INPUT_PATH)
+	solver := Puzzle{}
+	_, answer2 := solver.Solve(testInput)
+
+	assert.Equal(t, PART_2_EXPECTED, answer2)
+}
