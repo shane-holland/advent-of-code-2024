324dfaf2c915c5f604ee583bb91b53dd62083594
Shane Holland <shane.holland@ahead.com>
2024-12-15 12:26:10 -0500

    Refactor
    - Removed redundant code
    - Attempted to implement a stack data structure... clumsily.

--- solution/day-15/main.go
+++ solution/day-15/main.go
@@ -21,7 +21,7 @@ func (d Puzzle) Solve(input string) (string, string) {
 
 /* -------------------------------- Solution -------------------------------- */
 
-// Part 1:
+// Part 1: Return the sum of the GPS coordinates of all boxes in the warehouse.
 func part1(input string) string {
 	warehouse := parseWarehouse(input)
 	for warehouse.NextInstruction() {
@@ -30,14 +30,14 @@ func part1(input string) string {
 	return fmt.Sprintf("%d", warehouse.BoxGpsSum())
 }
 
-// Part 2:
+// Part 2:Return the sum of the GPS coordinates of all boxes in the wide warehouse.
 func part2(input string) string {
-	warehouse := parseWideWarehouse(input)
+	warehouse := parseWarehouse(input)
+	warehouse.Expand()
 	for warehouse.NextInstruction() {
 		// Move boxes until out of instructions
-		// warehouse.Draw()
 	}
-	// warehouse.Draw()
+
 	return fmt.Sprintf("%d", warehouse.BoxGpsSum())
 }
 
@@ -45,13 +45,14 @@ func part2(input string) string {
 
 // Part 1
 const FREE = 0
-const BOX = 1
-const WALL = 2
+const WALL = 1
+const BOX = 2
 
 // Part 2
 const BOX_LEFT = 3
 const BOX_RIGHT = 4
 
+// Directions Map
 var Directions = map[string]util.Point{
 	"^": {X: 0, Y: -1},
 	"v": {X: 0, Y: 1},
@@ -61,32 +62,62 @@ var Directions = map[string]util.Point{
 
 /* -------------------- Warehouse Definition and Methods -------------------- */
 
+// Represents a warehouse with wall and box positions, robot position, instructions, and expanded status.
 type Warehouse struct {
 	Map          [][]int
 	Robot        util.Point
 	Instructions []util.Point
+	Expanded     bool
 }
 
+// Returns the GPS value of a Box based on it's location in the warehouse.
 func (w Warehouse) Gps(point util.Point) int {
 	return point.X + (100 * point.Y)
 }
 
-func (w Warehouse) Draw() {
+// Returns the sum of the GPS coordinates of all boxes in the warehouse.
+func (w Warehouse) BoxGpsSum() int {
+	comparator := BOX
+	if w.Expanded {
+		comparator = BOX_LEFT
+	}
+
+	sum := 0
 	for y := 0; y < len(w.Map); y++ {
 		for x := 0; x < len(w.Map[y]); x++ {
-			switch w.Map[y][x] {
-			case WALL:
-				print("#")
-			case FREE:
-				print(".")
-			case BOX:
-				print("O")
+			if w.Map[y][x] == comparator {
+				sum += w.Gps(util.Point{X: x, Y: y})
 			}
 		}
-		println()
 	}
+	return sum
 }
 
+// Expands the warehouse by doubling the width of each row.
+func (w *Warehouse) Expand() {
+	expandedMap := make([][]int, len(w.Map))
+	for y := 0; y < len(w.Map); y++ {
+		expandedMap[y] = make([]int, len(w.Map[y])*2)
+		for x := 0; x < len(w.Map[y]); x++ {
+			if w.Map[y][x] == BOX {
+				expandedMap[y][x*2] = BOX_LEFT
+				expandedMap[y][x*2+1] = BOX_RIGHT
+			} else {
+				expandedMap[y][x*2] = w.Map[y][x]
+				expandedMap[y][x*2+1] = w.Map[y][x]
+			}
+		}
+	}
+	w.Map = expandedMap
+
+	// Adjust the robot's position
+	w.Robot.X *= 2
+	w.Map[w.Robot.Y][w.Robot.X*2+1] = FREE
+
+	w.Expanded = true
+}
+
+// Runs the next robot instruction in the warehouse.
 func (w *Warehouse) NextInstruction() bool {
 	if len(w.Instructions) == 0 {
 		return false
@@ -101,186 +132,113 @@ func (w *Warehouse) NextInstruction() bool {
 		// Free space, the robot can move
 		w.Robot.X += next.X
 		w.Robot.Y += next.Y
-	} else if w.Map[w.Robot.Y+next.Y][w.Robot.X+next.X] == BOX {
+	} else {
 		// Box in the way, see if the box can move
-		boxStack := []util.Point{{X: w.Robot.X + next.X, Y: w.Robot.Y + next.Y}}
-		for {
-			test := util.Point{X: boxStack[len(boxStack)-1].X + next.X, Y: boxStack[len(boxStack)-1].Y + next.Y}
-			if w.Map[test.Y][test.X] == WALL {
-				// We've hit a wall.  No movement is possible
-				boxStack = []util.Point{}
-				break
-			} else if w.Map[test.Y][test.X] == FREE {
-				// The robot and box stack can move
-				w.Robot.X += next.X
-				w.Robot.Y += next.Y
-				w.Map[w.Robot.Y][w.Robot.X] = FREE
-				break
-			} else if w.Map[test.Y][test.X] == BOX {
-				// Add the box to the stack
-				boxStack = append(boxStack, test)
-			}
-		}
-		for len(boxStack) > 0 {
-			w.Map[boxStack[0].Y+next.Y][boxStack[0].X+next.X] = BOX
-			boxStack = boxStack[1:]
+		boxStacks := w.getBoxesToPush(next)
+
+		// If there are boxes to push, push them
+		if len(boxStacks) > 0 {
+			w.pushBoxes(boxStacks, next)
+			// Update the robot's position
+			w.Robot.X += next.X
+			w.Robot.Y += next.Y
 		}
 	}
 	return true
 }
 
-func (w Warehouse) BoxGpsSum() int {
-	sum := 0
-	for y := 0; y < len(w.Map); y++ {
-		for x := 0; x < len(w.Map[y]); x++ {
-			if w.Map[y][x] == BOX {
-				sum += w.Gps(util.Point{X: x, Y: y})
+// Returns a stack of boxes that can be pushed in the direction of the next instruction.
+func (w Warehouse) getBoxesToPush(next util.Point) BoxStackStack {
+	boxStacks := BoxStackStack{w.getBox(util.Point{X: w.Robot.X + next.X, Y: w.Robot.Y + next.Y})}
+
+	for {
+		boxesAdded := false
+		testStack := boxStacks.Peek()
+		nextStack := make(BoxStack, 0)
+
+		for len(testStack) > 0 {
+			box := testStack.Pop()
+			test := util.Point{X: box.X + next.X, Y: box.Y + next.Y}
+			value := w.Map[test.Y][test.X]
+
+			if value == WALL {
+				// We've hit a wall.  No movement is possible
+				return make(BoxStackStack, 0)
+			} else if value >= BOX && !slices.Contains(testStack, test) {
+				if !slices.Contains(nextStack, test) {
+					nextStack = append(nextStack, w.getBox(test)...)
+					boxesAdded = true
+				}
 			}
 		}
+
+		if boxesAdded {
+			boxStacks.Push(nextStack)
+		} else {
+			break
+		}
 	}
-	return sum
+
+	return boxStacks
 }
 
-/* ------------------ WideWarehouse Definition and Methods ------------------ */
+// Returns a stack with all components of a box at a given position
+func (w Warehouse) getBox(position util.Point) BoxStack {
+	box := BoxStack{position}
+	// If this box is a double box, add the other side to the stack
+	if w.Map[position.Y][position.X] == BOX_LEFT {
+		box.Push(util.Point{X: position.X + 1, Y: position.Y})
+	} else if w.Map[position.Y][position.X] == BOX_RIGHT {
+		box.Push(util.Point{X: position.X - 1, Y: position.Y})
+	}
 
-type WideWarehouse struct {
-	Map          [][]int
-	Robot        util.Point
-	Instructions []util.Point
+	return box
 }
 
-func (w WideWarehouse) Draw() {
+// Pushes the boxes in the boxStacks in the direction of the next instruction.
+func (w *Warehouse) pushBoxes(boxStacks BoxStackStack, next util.Point) {
+	temp := make([][]int, len(w.Map))
+	for i, row := range w.Map {
+		temp[i] = slices.Clone(row)
+	}
+	for len(boxStacks) > 0 {
+		boxstack := boxStacks.Pop()
+		for len(boxstack) > 0 {
+			box := boxstack.Pop()
+			w.Map[box.Y][box.X] = FREE
+			w.Map[box.Y+next.Y][box.X+next.X] = temp[box.Y][box.X]
+		}
+	}
+}
+
+// Draws the warehouse to the console.
+func (w Warehouse) Draw() {
 	for y := 0; y < len(w.Map); y++ {
 		for x := 0; x < len(w.Map[y]); x++ {
 			switch w.Map[y][x] {
 			case WALL:
 				print("#")
+			case BOX:
+				print("O")
+			case BOX_LEFT:
+				print("[")
+			case BOX_RIGHT:
+				print("]")
 			case FREE:
 				if w.Robot.X == x && w.Robot.Y == y {
 					print("@")
 				} else {
 					print(".")
 				}
-			case BOX_LEFT:
-				print("[")
-			case BOX_RIGHT:
-				print("]")
 			}
 		}
 		println()
 	}
 }
 
-func (w WideWarehouse) Gps(point util.Point) int {
-	return point.X + (100 * point.Y)
-}
-
-func (w *WideWarehouse) NextInstruction() bool {
-	if len(w.Instructions) == 0 {
-		return false
-	}
-
-	next := w.Instructions[0]
-	w.Instructions = w.Instructions[1:]
-
-	if w.Map[w.Robot.Y+next.Y][w.Robot.X+next.X] == WALL {
-		// Do nothing
-	} else if w.Map[w.Robot.Y+next.Y][w.Robot.X+next.X] == FREE {
-		// Free space, the robot can move
-		w.Robot.X += next.X
-		w.Robot.Y += next.Y
-	} else if w.Map[w.Robot.Y+next.Y][w.Robot.X+next.X] > BOX {
-		// Box in the way, see if the box can move
-		boxes := []util.Point{{X: w.Robot.X + next.X, Y: w.Robot.Y + next.Y}}
-		if w.Map[w.Robot.Y+next.Y][w.Robot.X+next.X] == BOX_LEFT {
-			boxes = append(boxes, util.Point{X: w.Robot.X + next.X + 1, Y: w.Robot.Y + next.Y})
-		} else {
-			boxes = append(boxes, util.Point{X: w.Robot.X + next.X - 1, Y: w.Robot.Y + next.Y})
-		}
-		boxStacks := [][]util.Point{boxes}
-		for {
-			testStack := boxStacks[len(boxStacks)-1]
-			boxesAdded := false
-			wall := false
-			nextStack := []util.Point{}
-			for _, box := range testStack {
-				test := util.Point{X: box.X + next.X, Y: box.Y + next.Y}
-				if w.Map[test.Y][test.X] == WALL {
-					// We've hit a wall.  No movement is possible
-					wall = true
-					break
-				} else if w.Map[test.Y][test.X] > BOX && !slices.Contains(testStack, test) {
-					if !slices.Contains(nextStack, util.Point{X: test.X, Y: test.Y}) {
-						if w.Map[test.Y][test.X] == BOX_LEFT {
-							nextStack = append(nextStack, []util.Point{test, {X: test.X + 1, Y: test.Y}}...)
-						} else {
-							nextStack = append(nextStack, []util.Point{test, {X: test.X - 1, Y: test.Y}}...)
-						}
-						boxesAdded = true
-					}
-				}
-			}
-
-			if wall {
-				// We've hit a wall.  No movement is possible
-				boxStacks = [][]util.Point{}
-				break
-			}
-			if !boxesAdded {
-				// The robot and box stack can move
-				// w.Robot.X += next.X
-				// w.Robot.Y += next.Y
-				// w.Map[w.Robot.Y][w.Robot.X] = FREE
-				break
-			} else {
-				boxStacks = append(boxStacks, nextStack)
-			}
-		}
-		if (len(boxStacks) > 0) {
-			temp := make([][]int, len(w.Map))
-			for i, row := range w.Map {
-				temp[i] =slices.Clone(row)
-			}
-			for len(boxStacks)> 0 {
-				boxstack := boxStacks[len(boxStacks)-1]
-				boxStacks = boxStacks[:len(boxStacks)-1]
-				for len(boxstack) > 0 {
-					box := boxstack[len(boxstack)-1]
-					boxstack = boxstack[:len(boxstack)-1]
-					w.Map[box.Y][box.X] = FREE
-					w.Map[box.Y+next.Y][box.X+next.X] = temp[box.Y][box.X]
-				}
-			}
-			w.Robot.X += next.X
-			w.Robot.Y += next.Y
-			// if next.Y != 0 {
-			// 	if w.Map[w.Robot.Y][w.Robot.X+1] == BOX_RIGHT {
-			// 		w.Map[w.Robot.Y][w.Robot.X+1] = FREE
-			// 	}
-			// 	if w.Map[w.Robot.Y][w.Robot.X-1] == BOX_LEFT {
-			// 		w.Map[w.Robot.Y][w.Robot.X-1] = FREE
-			// 	}
-			// }
-			// w.Map[w.Robot.Y][w.Robot.X] = FREE
-		}
-	}
-	return true
-}
-
-func (w WideWarehouse) BoxGpsSum() int {
-	sum := 0
-	for y := 0; y < len(w.Map); y++ {
-		for x := 0; x < len(w.Map[y]); x++ {
-			if w.Map[y][x] == BOX_LEFT {
-				sum += w.Gps(util.Point{X: x, Y: y})
-			}
-		}
-	}
-	return sum
-}
-
 /* ----------------------------- Helper Methods ----------------------------- */
 
+// Parses the input string into a Warehouse struct.
 func parseWarehouse(input string) Warehouse {
 	robot := util.Point{}
 
@@ -300,6 +258,7 @@ func parseWarehouse(input string) Warehouse {
 				warehouseMap[y][x] = BOX
 			case "@":
 				robot = util.Point{X: x, Y: y}
+				warehouseMap[y][x] = FREE
 			}
 		}
 	}
@@ -310,39 +269,7 @@ func parseWarehouse(input string) Warehouse {
 		Instructions: parseInstructions(instructionsString)}
 }
 
-func parseWideWarehouse(input string) WideWarehouse {
-	robot := util.Point{}
-
-	mapGrid := strings.Split(strings.Split(input, "\n\n")[0], "\n")
-	instructionsString := strings.Split(input, "\n\n")[1]
-	var warehouseMap [][]int = make([][]int, len(mapGrid)*2)
-
-	for y := 0; y < len(mapGrid); y++ {
-		warehouseMap[y] = make([]int, len(mapGrid[y])*2)
-		for x := 0; x < len(mapGrid[y]); x++ {
-			switch string(mapGrid[y][x]) {
-			case "#":
-				warehouseMap[y][x*2] = WALL
-				warehouseMap[y][x*2+1] = WALL
-			case ".":
-				warehouseMap[y][x*2] = FREE
-				warehouseMap[y][x*2+1] = FREE
-			case "O":
-				warehouseMap[y][x*2] = BOX_LEFT
-				warehouseMap[y][x*2+1] = BOX_RIGHT
-			case "@":
-				robot = util.Point{X: x * 2, Y: y}
-				warehouseMap[y][x*2+1] = FREE
-			}
-		}
-	}
-
-	return WideWarehouse{
-		Map:          warehouseMap,
-		Robot:        robot,
-		Instructions: parseInstructions(instructionsString)}
-}
-
+// Parses the instructions string into an array of position deltas.
 func parseInstructions(input string) []util.Point {
 	instructions := []util.Point{}
 
@@ -351,4 +278,50 @@ func parseInstructions(input string) []util.Point {
 		instructions = append(instructions, Directions[direction])
 	}
 	return instructions
+}
+
+/* --------------------- BoxStacks Definition and Methods -------------------- */
+
+// BoxStack is a stack of points representing boxes.
+type BoxStack []util.Point
+
+// Push a point onto the stack.
+func (s *BoxStack) Push(v util.Point) {
+	*s = append(*s, v)
+}
+
+// Pop a point from the stack.
+func (s *BoxStack) Pop() util.Point {
+	l := len(*s)
+	val := (*s)[l-1]
+	*s = (*s)[:l-1]
+	return val
+}
+
+// Peek at the top of the stack.
+func (s BoxStack) Peek() util.Point {
+	l := len(s)
+	return s[l-1]
+}
+
+// BoxStackStack is a stack of BoxStacks.
+type BoxStackStack []BoxStack
+
+// Push a BoxStack onto the stack.
+func (s *BoxStackStack) Push(v BoxStack) {
+	*s = append(*s, v)
+}
+
+// Pop a BoxStack from the stack.
+func (s *BoxStackStack) Pop() BoxStack {
+	l := len(*s)
+	val := (*s)[l-1]
+	*s = (*s)[:l-1]
+	return val
+}
+
+// Peek at the top of the stack.
+func (s BoxStackStack) Peek() BoxStack {
+	l := len(s)
+	return s[l-1]
 }
